772f0ae6fdbb446464c4fc898508015c53c54ad0
Gopinath B <94794960+Gopz23@users.noreply.github.com>
2026-05-30 22:19:40 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+
+def clean_data(df):
+
+    df['MSRP'] = df['MSRP'].replace('[\\$,]', '', regex=True).astype(float)
+    df['Invoice'] = df['Invoice'].replace('[\\$,]', '', regex=True).astype(float)
+
+    df = df.dropna()
+
+    return df
